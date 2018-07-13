becd46a4dd1f60b32f6f27be9eced5dc051fd5fc
Dmitry <yashulyak@gmail.com>
2018-07-13 14:21:49 +0300

    Use marshalling from libp2p-peer library as it is expected in libp2p comonents

--- protocol_test.go
+++ protocol_test.go
@@ -1,22 +1,31 @@
 package ethv4
 
 import (
+	"crypto/ecdsa"
+	"math/rand"
 	"testing"
 
+	crypto "github.com/libp2p/go-libp2p-crypto"
+	peer "github.com/libp2p/go-libp2p-peer"
 	"github.com/stretchr/testify/require"
 
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/p2p/discover"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
 func TestETHv4(t *testing.T) {
-	key, _ := crypto.GenerateKey()
-	addr, err := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/34012/ethv4/" + discover.PubkeyID(&key.PublicKey).String())
+	pkey, pubkey, err := crypto.GenerateKeyPairWithReader(crypto.Secp256k1, 2048, rand.New(rand.NewSource(1)))
+	require.NoError(t, err)
+	pid, err := peer.IDFromPublicKey(pkey.GetPublic())
+	require.NoError(t, err)
+	addr, err := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/34012/ethv4/" + pid.Pretty())
 	require.NoError(t, err)
 	rst, err := addr.ValueForProtocol(P_ETHv4)
 	require.NoError(t, err)
-	nodeid, err := discover.HexID(rst)
+	nid, err := PeerIDToNodeID(rst)
 	require.NoError(t, err)
-	require.Equal(t, discover.PubkeyID(&key.PublicKey), nodeid)
+	require.Equal(t,
+		discover.PubkeyID((*ecdsa.PublicKey)(pubkey.(*crypto.Secp256k1PublicKey))),
+		nid,
+	)
 }
