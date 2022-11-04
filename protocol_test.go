635857a9cf68f207781e3e45e75a7aaa8144fd3a
Richard Ramos <info@richardramos.me>
2022-11-04 02:38:11 -0400

    chore: upgrade deps

--- protocol_test.go
+++ protocol_test.go
@@ -8,8 +8,8 @@ import (
 
 	btcec "github.com/btcsuite/btcd/btcec/v2"
 	"github.com/ethereum/go-ethereum/p2p/enode"
-	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
